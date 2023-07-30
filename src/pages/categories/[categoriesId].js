e91df65d4338c2eb9d4c8d5f57d7b882aba6585a
MD RAKIBUL ISLAM RAKIB <rakibulinux@gmail.com>
2023-07-30 17:46:14 +0600

    [Done] Categories

--- src/pages/categories/[categoriesId].js
+++ src/pages/categories/[categoriesId].js
@@ -1,9 +1,13 @@
 import RootLayout from "@/components/Layout/RootLayout";
+import ProductCard from "@/components/ProductCard";
 
-const SingleCategory = () => {
+const SingleCategory = ({ products }) => {
+  console.log(products);
   return (
-    <div className="min-h-screen">
-      <h1>I am single</h1>
+    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {products?.map((product) => (
+        <ProductCard key={product?.id} product={product} />
+      ))}
     </div>
   );
 };
@@ -11,4 +15,27 @@ const SingleCategory = () => {
 export default SingleCategory;
 SingleCategory.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(
+    "https://pc-builder-server-three.vercel.app/categories"
+  );
+  const categories = await res.json();
+  const paths = categories?.map((category) => ({
+    params: { categoriesId: category },
+  }));
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(
+    `https://pc-builder-server-three.vercel.app/products?category=${params.categoriesId}`
+  );
+  const products = await res.json();
+  return {
+    props: {
+      products,
+    },
+  };
 };
