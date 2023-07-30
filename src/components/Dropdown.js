b96b421e7f412c5ddb51ee8739cac937411c7242
MD RAKIBUL ISLAM RAKIB <rakibulinux@gmail.com>
2023-07-30 17:39:00 +0600

    [Done] Categories added

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,31 +4,31 @@ import React from "react";
 const items = [
   {
     key: "1",
-    label: <Link href="/cpu_processor">Cpu Processor</Link>,
+    label: <Link href="/categories/cpu_processor">Cpu Processor</Link>,
   },
   {
     key: "2",
-    label: <Link href="/motherboard">Motherboard</Link>,
+    label: <Link href="/categories/motherboard">Motherboard</Link>,
   },
   {
     key: "3",
-    label: <Link href="/ram">Ram</Link>,
+    label: <Link href="/categories/ram">Ram</Link>,
   },
   {
     key: "4",
-    label: <Link href="/power_supply_unit">Power Supply Unit</Link>,
+    label: <Link href="/categories/power_supply_unit">Power Supply Unit</Link>,
   },
   {
     key: "5",
-    label: <Link href="/storage_device">Storage Device</Link>,
+    label: <Link href="/categories/storage_device">Storage Device</Link>,
   },
   {
     key: "6",
-    label: <Link href="/monitor">Monitor</Link>,
+    label: <Link href="/categories/monitor">Monitor</Link>,
   },
   {
     key: "7",
-    label: <Link href="/others">others</Link>,
+    label: <Link href="/categories/others">others</Link>,
   },
 ];
 const DropdownMenu = () => {
--- src/pages/categories/[categoriesId].js
+++ src/pages/categories/[categoriesId].js
@@ -1,9 +1,14 @@
+import RootLayout from "@/components/Layout/RootLayout";
+
 const SingleCategory = () => {
   return (
-    <div>
+    <div className="min-h-screen">
       <h1>I am single</h1>
     </div>
   );
 };
 
 export default SingleCategory;
+SingleCategory.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
